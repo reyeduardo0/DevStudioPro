63a4c2f95215a6cec0708e0d066c739578a63a29
reyeduardo0 <reyeduardo0@gmail.com>
2026-04-21 11:49:30 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,10 @@ import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// En ESM (ECMAScript Modules), __dirname no está definido por defecto.
-// Estas líneas lo emulan para que path.resolve funcione correctamente.
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 export default defineConfig(({ mode }) => {
-  // Carga las variables de entorno basadas en el 'mode' (development, production, etc.)
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
@@ -18,17 +16,14 @@ export default defineConfig(({ mode }) => {
       tailwindcss(),
     ],
     define: {
-      // Expone la variable al frontend
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
     resolve: {
       alias: {
-        // Establece '@' como alias para la raíz o la carpeta 'src'
         '@': path.resolve(__dirname, './src'), 
       },
     },
     server: {
-      // Mantiene la lógica para deshabilitar HMR si la variable existe
       hmr: process.env.DISABLE_HMR !== 'true',
     },
   };
